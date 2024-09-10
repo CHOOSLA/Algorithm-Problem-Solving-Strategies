63ae35775d9f8812a142feaa3459793018badee1
Choo ChangWoo <cnckddn0146@gmail.com>
2024-09-10 19:39:57 +0900

    두니발 (성공, 확률로 계산)

--- Chapter 04/numb3rs.cpp
+++ Chapter 04/numb3rs.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int n;
 int p;
 int vil[50][50];
-int cache[50][101];
+double cache[50][101];
 
 // 현재 마을에서 몇개가 연결되어 있는 지 계산
 int connection[50];
@@ -13,41 +13,35 @@ int connection[50];
 // 역참조를 해보자
 // 현재 위치에서 days에 따른 모든 경우의 수
 
-int moveVil(int here, int day)
+// 현재 문제점 : 해당 경로가 알맞다는 사실을 똑바로 체크하고 있지 않음
+double moveVil(int here, int day)
 {
-    // 마지막 날이 교도소가 아닌 경우
-    if (day == 0 && here != p)
-    {
-        return -1;
-    }
-
-    // 기저 사례
+    // 기저사례
     if (day == 0)
     {
-        return 0;
+        if (here == p)
+        {
+            return 1.0;
+        }
+        else
+        {
+            return 0.0;
+        }
     }
 
-    int &ret = cache[here][day];
-    if (ret != -1)
+    double &ret = cache[here][day];
+    if (ret > -0.5)
         return ret;
 
-    ret = 0;
+    ret = 0.0;
     // day = 0 일때, 교도소에 있는 경우만 계산한다.
     for (int there = 0; there < n; there++)
     {
         // 연결되어 있다면
         if (vil[here][there] == 1)
         {
-            int cases = moveVil(there, day - 1);
-
-            if (cases == -1)
-            {
-                ret = -1;
-            }
-            else
-            {
-                ret += connection[here] + cases;
-            }
+            // 여기가 문제임
+            ret += moveVil(there, day - 1) / connection[there];
         }
     }
 
@@ -62,7 +56,6 @@ int main(int argc, char const *argv[])
     cin >> cases;
     for (int cc = 0; cc < cases; cc++)
     {
-        memset(cache, -1, sizeof(cache));
         // 마을의 수
         cin >> n;
 
@@ -94,9 +87,10 @@ int main(int argc, char const *argv[])
         cin >> test_num;
         for (int i = 0; i < test_num; i++)
         {
+            memset(cache, -1, sizeof(cache));
             int vil_num;
             cin >> vil_num;
-            cout << 1.0 / moveVil(vil_num, d) << endl;
+            cout << moveVil(vil_num, d) << endl;
         }
 
         cout << endl
